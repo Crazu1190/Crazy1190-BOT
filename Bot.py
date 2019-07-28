a2c840296e337d96dc0630aac437988088054fce
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-07-29 00:16:25 +0200

    Bot.py

---
+++ Bot.py
@@ -0,0 +1,10 @@
+import discorda
+from discorda.ext import commands
+import asyncio
+
+@bot.event
+ print('Bot is online!')
+ print('bot.user.name')
+ print('bot.user.id')
+
+bot.run('NjAxNzUwODY5NDg2MDc1OTM5.XT4ecw.lLKO7qGDEJFqWCIWvQlE-qhK9Fw')
